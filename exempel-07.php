eb97354b6e91f156aef56b63fcbe2f16972395ac
Daniel Håkansson <daniel.hakansson@yh.nackademin.se>
2021-01-11 12:04:11 +0100

    push

---
+++ exempel-07.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1); ?>
+
+<h1>Exempel på strikta typer</h1>
+
+
+<?php
+
+function addNumbers(int $a , int $b) : int{
+    return $a + $b ;
+}
+
+
+echo addNumbers(11,22);
+
+?>
+
+
+
---
+++ exempel-10.php
@@ -0,0 +1,11 @@
+<?php
+
+if(strtoupper($_SERVER['REQUEST_METHOD']) != 'POST'){
+    header('Location: index.php');
+    http_response_code(400);
+    exit('Bad Request');
+}
+
+        echo "Hello " . $_POST['firstname'];
+
+?>
